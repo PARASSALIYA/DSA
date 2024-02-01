f901e1ffbb3fa77d30e782ae96faaa1e03f183ee
PARASSALIYA <parassaliya123@gmail.com>
2024-02-01 23:35:24 +0530

    1/2/2024

---
+++ DSA-1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter A :";
+    cin >> a;
+    cout << "Enter B :";
+    cin >> b;
+
+    cout << "sum : " << a + b;
+}
